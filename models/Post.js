04a813d738ed62223805dfc9f85d6f3bc3c8bd78
rafiqul-bashar <rafiqul.bashar@outlook.com>
2022-02-09 14:14:15 +0600

    ss

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema(
     {
     user: { type: String, required: true},
-    email: { type: String, required: true},
+body: { type: String, required: true},
     likes: { type: Number},
     comment: [{ body: String, date: Date,name:String }],
 
